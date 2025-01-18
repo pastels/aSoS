7c8fe4634c1dce4765e33d3ddd0f8b55701e6211
pastels <eszto98@gmail.com>
2025-01-18 15:57:39 +0100

    started the movement script

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("MouseX X") * Time.deltaTime; *sensX;
+        float mouseY = Input.GetAxisRaw("MouseX Y") * Time.deltaTime; *sensY;
+
+        yRotation += mouseX;
+
+        xRotation += mouseY;
     }
 }
